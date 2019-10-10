c1c7b9efc4831030ef524a2aa2a4608f7bcb01b9
Land Sea <45856047+wobushiludahai@users.noreply.github.com>
2019-10-10 16:11:09 +0800

    Create 1066.c

---
+++ 1066.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int main(void)
+{
+    int numM, numN, numA, numB, value;
+    int testbuf[1000][500] = {0};
+
+
+    scanf("%d %d %d %d %d", &numM, &numN, &numA, &numB, &value);
+
+    for(int i = 0; i < numM; i++)
+    {
+        for(int j = 0; j < numN; j++)
+        {
+            scanf("%d", &testbuf[i][j]);
+        }
+    }
+
+    // for(int i = 0; i < numM; i++)
+    // {
+    //     printf("\n");
+    //     for(int j = 0; j < numN; j++)
+    //     {
+    //         printf("%d ", testbuf[i][j]);
+    //     }
+    // }
+    // printf("\n"); 
+    // printf("%d %d\n", numA, numB);   
+
+    for(int i = 0; i < numM; i++)
+    {
+        for(int j = 0; j < numN; j++)
+        {
+            if((testbuf[i][j] >= numA)&&(testbuf[i][j] <= numB))
+            {
+                printf("%03d", value);
+            }
+            else
+            {
+                printf("%03d", testbuf[i][j]);
+            }
+            if(j!=(numN - 1))
+            {
+                printf(" ");
+            }
+            else
+            {
+                printf("\n");
+            }
+        }
+    }
+
+    return 0;
+}
